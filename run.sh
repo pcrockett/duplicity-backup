b57e3eea2a8255f1dec7f93872a4f3c8b58de4f5
Philip Crockett <contact@philcrockett.com>
2019-07-14 20:03:08 +0200

    Mount / backup / unmount for each share

--- run.sh
+++ run.sh
@@ -13,47 +13,68 @@ fi
 
 source "$VARS_SCRIPT"
 
-mount_all() {
+mount_share() {
 
-  for share in ${SHARES[@]}; do
+  share="$1"
 
-    if [ ! -d "$MOUNT_DIR/$share" ]; then
-      mkdir "$MOUNT_DIR/$share"
-    fi
+  if [ ! -d "$MOUNT_DIR/$share" ]; then
+    mkdir "$MOUNT_DIR/$share"
+  fi
 
-    if mountpoint -q "$MOUNT_DIR/$share"; then
-      continue
-    fi
-
+  if mountpoint -q "$MOUNT_DIR/$share"; then
+    # Do nothing; already mounted
+  else
     mount -t cifs \
       -o "username=$NAS_USERNAME,password=$NAS_PASSWORD,vers=2.0,file_mode=0400,dir_mode=0500,gid=$BACKUP_GID,uid=$BACKUP_UID" \
       "//$NAS_HOST/$share" "$MOUNT_DIR/$share"
-  done
-
+  fi
 }
 
-unmount_all() {
-  for share in ${SHARES[@]}; do
+unmount_share() {
+
+  share="$1"
+
+  if mountpoint -q "$MOUNT_DIR/$share"; then
     umount "//$NAS_HOST/$share"
-  done
+  fi
 }
 
-run_backup() {
+backup_share() {
+
+  share="$1"
+
   sudo -u "$BACKUP_USER" \
     PASSPHRASE="$GPG_PASSPHRASE" \
     SIGN_PASSPHRASE="$GPG_PASSPHRASE" \
-    duplicity "$MOUNT_DIR" "b2://$B2_KEY_ID:$B2_KEY@$B2_BUCKET" \
+    duplicity "$MOUNT_DIR" "b2://$B2_KEY_ID:$B2_KEY@$B2_BUCKET/$share/" \
       --sign-key "$GPG_SIGN_KEY" \
       --encrypt-key "$GPG_ENCR_KEY!" \
       --encrypt-key "$GPG_OFFLINE_KEY!"
 }
 
-mount_all
+run_backup() {
+  
+  backup_status=0
 
-if run_backup; then
-  unmount_all
-  exit 0
-else
-  unmount_all
-  exit 1
-fi
+  for share in ${SHARES[@]}; do
+
+    echo "Backing up $share..."
+
+    mount_share $share
+    
+    if backup_share $share; then
+      echo "$share backed up successfully."
+    else
+      # Failure. Make sure we exit this function with a failed exit code
+      backup_status=1
+      echo "$share backup failure."
+    fi
+
+    unmount_share $share
+    
+  done
+
+  return $backup_status
+}
+
+run_backup
